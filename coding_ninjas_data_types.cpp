527835018d1f0bee83a1bc5022b05434b59e7616
MICAH JOSEPH <64728507+micahjoseph@users.noreply.github.com>
2022-01-10 11:02:32 +0530

    Add files via upload

---
+++ coding_ninjas_data_types.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=10;
+	int b=23;
+	int c=a+b;
+	cout<<c<<endl;
+	char d='x';
+	float f=1.23;
+	bool bo =true;
+	int size = sizeof(bo);
+	cout<<size<<endl;
+	cout<<bo;
+	
+}
---
+++ data_types.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	char c;
+	a=199987;
+	c=a;
+	cout<<c;
+	
+	float f=1.2;
+	a=f;
+	cout<<a<<endl;
+	
+	int i=12;
+	char w='a';
+	int t=i+c;
+	char y=i+c;
+	cout<<t<<y<<endl;
+	
+	
+}
